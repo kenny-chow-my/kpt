fda084c1a900498c17bc5e72645d10bc1f482e1d
Kenny Chow <kenny@chow.my>
2023-07-19 21:53:44 +0800

    Added CORS to make the service work with a separate frontend

--- src/main/java/com/kenny/llmdemo/kpt/config/SecurityConfig.java
+++ src/main/java/com/kenny/llmdemo/kpt/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.kenny.llmdemo.kpt.config;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -21,10 +26,12 @@ public class SecurityConfig {
         OidcUserService googleUserService = new OidcUserService();
         googleUserService.setAccessibleScopes(googleScopes);
 
-        http.csrf().disable()
+        http.csrf().disable().cors().and()
+
                 .authorizeRequests()
                 .requestMatchers("/translate/**").permitAll()
                 .requestMatchers("/language-pair/**").permitAll()
+                .requestMatchers(HttpMethod.OPTIONS,  "/**").permitAll()
                 .requestMatchers("/user/**").authenticated()
                 .anyRequest().permitAll();
 
@@ -40,5 +47,17 @@ public class SecurityConfig {
 //                .oauth2Login(oauthLogin -> oauthLogin.userInfoEndpoint()
 //                        .oidcUserService(googleUserService));
 //        return http.build();
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/kenny/llmdemo/kpt/controller/LanguagePairController.java
+++ src/main/java/com/kenny/llmdemo/kpt/controller/LanguagePairController.java
@@ -8,14 +8,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/language-pair")
 public class LanguagePairController {
